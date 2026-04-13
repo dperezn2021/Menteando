23441a879385e118fcf49b2a4e611b46ed6bf008
Daniel Pérez Navarro <122210965+dperezn2021@users.noreply.github.com>
2026-04-13 20:19:14 +0200

    Delete Unity_Menteando directory

--- Unity_Menteando/Assets/Juegos/ScriptsGenerales/Metricas.cs
+++
@@ -1,9 +0,0 @@
-[System.Serializable]
-public class Metricas
-{
-    public int totalTrials;
-    public int correctResponses;
-    public int commissionErrors;
-    public int omissionErrors;
-    public float avgReactionTime;
-}
--- Unity_Menteando/Assets/Juegos/ScriptsGenerales/SesionJuego.cs
+++
@@ -1,8 +0,0 @@
-[System.Serializable]
-public class SesionJuego
-{
-    public string playerId;
-    public string game;
-    public Metricas metrics;
-    public float sessionDuration;
-}
