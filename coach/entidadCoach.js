9dfebf9be4be209c58ff804a3d881454c3f77b3c
dperezn2021 <d.perezn.2021@alumnos.urjc.es>
2026-04-16 17:43:24 +0200

    v0.0.14

--- coach/entidadCoach.js
+++ coach/entidadCoach.js
@@ -2,16 +2,20 @@ function CoachEntity(element, bubble) {
     this.element = element;
     this.bubble = bubble;
     this.visible = false;
+    this.pointerEvents = "none";
 }
 
 CoachEntity.prototype.show = function(msg) {
     this.bubble.innerText = msg;
+    if (this.element && this.element.style) this.element.style.pointerEvents = "auto"; // permitir clics cuando visible
     this.element.classList.add("visible");
     this.visible = true;
 };
 
 CoachEntity.prototype.hide = function() {
     this.element.classList.remove("visible");
+    if (this.element && this.element.style) this.element.style.pointerEvents = "none"; // no captura clics cuando está oculto
+
     this.visible = false;
 };
 
